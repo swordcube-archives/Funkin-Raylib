1da03fffad18d0f80bc3fb1a2ff0df21c592da6f
Leather128 <81660872+Leather128@users.noreply.github.com>
2023-03-26 17:44:46 -0400

    did some "fixing" for linux

--- src/funkin/helpers/Paths.hx
+++ src/funkin/helpers/Paths.hx
@@ -4,100 +4,94 @@ import sys.FileSystem;
 import engine.utilities.Atlas;
 
 class Paths {
-    public static final scriptExtensions:Array<String> = [
-        "hx",
-        "hxs",
-        "hsc",
-        "hscript"
-    ];
+	public static final scriptExtensions:Array<String> = ["hx", "hxs", "hsc", "hscript"];
 
-    public static function asset(path:String) {
-        var srcPath:String = '../../assets/$path';
-        if(FileSystem.exists(srcPath))
-            return srcPath;
+	public static function asset(path:String) {
+		var srcPath:String = '../../assets/$path';
+		if (FileSystem.exists(srcPath))
+			return srcPath;
 
-        return './assets/$path';
-    }
+		return './assets/$path';
+	}
 
-    public inline static function image(path:String) {
-        return asset('images/$path.png');
-    }
+	public inline static function image(path:String) {
+		return asset('images/$path.png');
+	}
 
-    public inline static function music(path:String) {
-        return asset('music/$path.ogg');
-    }
+	public inline static function music(path:String) {
+		return asset('music/$path.ogg');
+	}
 
-    public inline static function sound(path:String) {
-        return asset('sounds/$path.ogg');
-    }
+	public inline static function sound(path:String) {
+		return asset('sounds/$path.ogg');
+	}
 
-    public static function songInst(song:String, ?diff:String = "normal") {
-        var songPaths:Array<String> = [
-            asset('songs/${song.toLowerCase()}/Inst-$diff.ogg')
-        ];
-        for(p in songPaths) {
-            if(FileSystem.exists(p))
-                return p;
-        }
-        var p:String = asset('songs/${song.toLowerCase()}/Inst.ogg');
-        return p;
-    }
+	public static function songInst(song:String, ?diff:String = "normal") {
+		var songPaths:Array<String> = [asset('songs/${song.toLowerCase()}/Inst-$diff.ogg')];
+		for (p in songPaths) {
+			if (FileSystem.exists(p))
+				return p;
+		}
+		var p:String = asset('songs/${song.toLowerCase()}/Inst.ogg');
+		return p;
+	}
 
-    public static function songVoices(song:String, ?diff:String = "normal") {
-        var songPaths:Array<String> = [
-            asset('songs/${song.toLowerCase()}/Voices-$diff.ogg')
-        ];
-        for(p in songPaths) {
-            if(FileSystem.exists(p))
-                return p;
-        }
-        var p:String = asset('songs/${song.toLowerCase()}/Voices.ogg');
-        return p;
-    }
+	public static function songVoices(song:String, ?diff:String = "normal") {
+		var songPaths:Array<String> = [asset('songs/${song.toLowerCase()}/Voices-$diff.ogg')];
+		for (p in songPaths) {
+			if (FileSystem.exists(p))
+				return p;
+		}
+		var p:String = asset('songs/${song.toLowerCase()}/Voices.ogg');
+		return p;
+	}
 
-    public static function songJson(song:String, ?diff:String = "normal") {
-        var songPaths:Array<String> = [
-            asset('songs/$song/$song-$diff.json'),
-            asset('data/$song/$song-$diff.json'),
-            asset('data/$song/$diff.json'),
-            asset('data/charts/$song/$diff.json'),
-            asset('data/charts/$song/$song-$diff.json'),
-        ];
-        for(p in songPaths) {
-            if(FileSystem.exists(p))
-                return p;
-        }
-        var p:String = asset('songs/$song/$diff.json');
-        return p;
-    }
+	public static function songJson(song:String, ?diff:String = "normal") {
+		song = song.toLowerCase();
+		diff = diff.toLowerCase();
 
-    public inline static function font(path:String) {
-        return asset('fonts/$path');
-    }
+		var songPaths:Array<String> = [
+			asset('songs/$song/$song-$diff.json'),
+			asset('data/$song/$song-$diff.json'),
+			asset('data/$song/$diff.json'),
+			asset('data/charts/$song/$diff.json'),
+			asset('data/charts/$song/$song-$diff.json'),
+		];
+		for (p in songPaths) {
+			if (FileSystem.exists(p))
+				return p;
+		}
+		var p:String = asset('songs/$song/$diff.json');
+		return p;
+	}
 
-    public inline static function xml(path:String) {
-        return asset('$path.xml');
-    }
+	public inline static function font(path:String) {
+		return asset('fonts/$path');
+	}
 
-    public inline static function txt(path:String) {
-        return asset('$path.txt');
-    }
+	public inline static function xml(path:String) {
+		return asset('$path.xml');
+	}
 
-    public static function script(path:String) {
-        for(ext in scriptExtensions) {
-            var p:String = asset('$path.$ext');
-            if(FileSystem.exists(p))
-                return p;
-        }
-        var p:String = asset('$path.hx');
-        return p;
-    }
+	public inline static function txt(path:String) {
+		return asset('$path.txt');
+	}
 
-    public inline static function json(path:String) {
-        return asset('$path.json');
-    }
+	public static function script(path:String) {
+		for (ext in scriptExtensions) {
+			var p:String = asset('$path.$ext');
+			if (FileSystem.exists(p))
+				return p;
+		}
+		var p:String = asset('$path.hx');
+		return p;
+	}
 
-    public inline static function getSparrowAtlas(path:String) {
-        return Atlas.fromSparrow(image(path), xml('images/$path'));
-    }
+	public inline static function json(path:String) {
+		return asset('$path.json');
+	}
+
+	public inline static function getSparrowAtlas(path:String) {
+		return Atlas.fromSparrow(image(path), xml('images/$path'));
+	}
 }
--- src/scenes/PlayState.hx
+++ src/scenes/PlayState.hx
@@ -19,261 +19,269 @@ import engine.Camera;
 import engine.Object;
 import engine.Game;
 import engine.Sprite;
+import engine.utilities.Logs;
 
 class PlayState extends MusicBeatScene {
-    public static var current:PlayState;
-    public static var SONG:SongData;
+	public static var current:PlayState;
+	public static var SONG:SongData;
 
-    public var startingSong:Bool = true;
-    public var endingSong:Bool = false;
+	public var startingSong:Bool = true;
+	public var endingSong:Bool = false;
 
-    public var inst:MusicEx;
-    public var vocals:MusicEx;
+	public var inst:MusicEx;
+	public var vocals:MusicEx;
 
-    public var startTimer:Timer;
+	public var startTimer:Timer;
 
-    public var cpuStrums:TypedGroup<Receptor>;
-    public var playerStrums:TypedGroup<Receptor>;
-    public var notes:NoteField;
+	public var cpuStrums:TypedGroup<Receptor>;
+	public var playerStrums:TypedGroup<Receptor>;
+	public var notes:NoteField;
 
-    public var camHUD:Camera;
-    public var camFollow:Object;
+	public var camHUD:Camera;
+	public var camFollow:Object;
 
-    public var unspawnNotes:Array<Note> = [];
-    public var scrollSpeed:Float = 2.7;
-    public var defaultCamZoom:Float = 0.9;
+	public var unspawnNotes:Array<Note> = [];
+	public var scrollSpeed:Float = 2.7;
+	public var defaultCamZoom:Float = 0.9;
 
-    public var health:Float = 1;
-    public var maxHealth:Float = 2;
+	public var health:Float = 1;
+	public var maxHealth:Float = 2;
 
-    public var scripts:Array<HScript> = [];
+	public var scripts:Array<HScript> = [];
 
-    override function create() {
-        super.create();
-        current = this;
+	override function create() {
+		super.create();
+		current = this;
 
-        Game.sound.music.stop();
+		Game.sound.music.stop();
 
-        Conductor.mapBPMChanges(SONG);
-        Conductor.changeBPM(SONG.bpm);
-        Conductor.position = Conductor.crochet * -5;
+		Conductor.mapBPMChanges(SONG);
+		Conductor.changeBPM(SONG.bpm);
+		Conductor.position = Conductor.crochet * -5;
 
-        if(SONG.keyCount == null) SONG.keyCount = 4;
-        if(SONG.stage == null) SONG.stage = "stage";
+		if (SONG.keyCount == null)
+			SONG.keyCount = 4;
+		if (SONG.stage == null)
+			SONG.stage = "stage";
 
-        scripts.push(new HScript(Paths.script('data/stages/${SONG.stage}')));
-        scripts.push(new HScript(Paths.script("data/scripts/testing")));
-        for (script in scripts) {
-            script.setParent(this);
-            script.call("onCreate");
-        }
-        Game.camera.zoom = defaultCamZoom;
+		scripts.push(new HScript(Paths.script('data/stages/${SONG.stage}')));
+		scripts.push(new HScript(Paths.script("data/scripts/testing")));
+		for (script in scripts) {
+			script.setParent(this);
+			script.call("onCreate");
+		}
+		Game.camera.zoom = defaultCamZoom;
 
-        Game.sound.list.push(inst = new MusicEx(Paths.songInst(SONG.song), 1, false));
-        inst.onComplete = endSong;
-        inst.pitch = Game.timeScale;
+		Game.sound.list.push(inst = new MusicEx(Paths.songInst(SONG.song), 1, false));
+		inst.onComplete = endSong;
+		inst.pitch = Game.timeScale;
 
-        Game.sound.list.push(vocals = new MusicEx(Paths.songVoices(SONG.song), 1, false));
-        vocals.pitch = Game.timeScale;
+		Game.sound.list.push(vocals = new MusicEx(Paths.songVoices(SONG.song), 1, false));
+		vocals.pitch = Game.timeScale;
 
-        Game.cameras.add(camHUD = new Camera());
+		Game.cameras.add(camHUD = new Camera());
 
-        add(cpuStrums = new TypedGroup<Receptor>());
-        add(playerStrums = new TypedGroup<Receptor>());
+		add(cpuStrums = new TypedGroup<Receptor>());
+		add(playerStrums = new TypedGroup<Receptor>());
 
-        add(notes = new NoteField());
-        notes.cameras = [camHUD];
+		add(notes = new NoteField());
+		notes.cameras = [camHUD];
 
-        var strumY:Float = (SettingsAPI.downscroll) ? Game.height - 160 : 50;
-        var receptorOffset:Float = 90;
+		var strumY:Float = (SettingsAPI.downscroll) ? Game.height - 160 : 50;
+		var receptorOffset:Float = 90;
 
-        for(i in 0...SONG.keyCount) {
-            // CPU receptor
-            var receptor = new Receptor((Note.swagWidth * i) + receptorOffset, strumY, SONG.keyCount, i);
-            receptor.cameras = [camHUD];
-            receptor.alpha = 0;
-            Tween.tween(receptor, {alpha: 1}, 0.5, {ease: Ease.circOut, startDelay: 0.3 * i});
-            cpuStrums.add(receptor);
-            receptor.animation.finishCallback = (name:String) -> {
-                if(name == "confirm")
-                    receptor.playAnim("static", true);
-            };
+		for (i in 0...SONG.keyCount) {
+			// CPU receptor
+			var receptor = new Receptor((Note.swagWidth * i) + receptorOffset, strumY, SONG.keyCount, i);
+			receptor.cameras = [camHUD];
+			receptor.alpha = 0;
+			Tween.tween(receptor, {alpha: 1}, 0.5, {ease: Ease.circOut, startDelay: 0.3 * i});
+			cpuStrums.add(receptor);
+			receptor.animation.finishCallback = (name:String) -> {
+				if (name == "confirm")
+					receptor.playAnim("static", true);
+			};
 
-            // Player receptor
-            var receptor = new Receptor((Note.swagWidth * i) + ((Game.width * 0.5) + receptorOffset), strumY, SONG.keyCount, i);
-            receptor.cameras = [camHUD];
-            receptor.alpha = 0;
-            Tween.tween(receptor, {alpha: 1}, 0.5, {ease: Ease.circOut, startDelay: 0.3 * i});
-            if(SettingsAPI.botplay) {
-                receptor.animation.finishCallback = (name:String) -> {
-                    if(name == "confirm")
-                        receptor.playAnim("static", true);
-                };
-            }
-            playerStrums.add(receptor);
-        }
+			// Player receptor
+			var receptor = new Receptor((Note.swagWidth * i) + ((Game.width * 0.5) + receptorOffset), strumY, SONG.keyCount, i);
+			receptor.cameras = [camHUD];
+			receptor.alpha = 0;
+			Tween.tween(receptor, {alpha: 1}, 0.5, {ease: Ease.circOut, startDelay: 0.3 * i});
+			if (SettingsAPI.botplay) {
+				receptor.animation.finishCallback = (name:String) -> {
+					if (name == "confirm")
+						receptor.playAnim("static", true);
+				};
+			}
+			playerStrums.add(receptor);
+		}
 
-        unspawnNotes = ChartParser.parseNotes(SONG);
+		unspawnNotes = ChartParser.parseNotes(SONG);
 
-        camFollow = new Object(0, 0);
-        camFollow.setPosition(770 + 411 / 2 - 100, 450 + 412 / 2 - 100); // 875.5, 556
-        Game.camera.target = camFollow;
-        Game.camera.followLerp = 0.04;
-        
-        startCountdown();
+		camFollow = new Object(0, 0);
+		camFollow.setPosition(770 + 411 / 2 - 100, 450 + 412 / 2 - 100); // 875.5, 556
+		Game.camera.target = camFollow;
+		Game.camera.followLerp = 0.04;
 
-        call("onCreatePost");
-    }
+		startCountdown();
 
-    public function startCountdown() {
-        var swagCounter:Int = 0;
-        startTimer = new Timer().start(Conductor.crochet / 1000, (timer:Timer) -> {
-            switch(swagCounter++) {
-                case 0: Game.sound.play(Paths.sound("game/countdown/base/intro3"));
-                case 1: Game.sound.play(Paths.sound("game/countdown/base/intro2"));
-                case 2: Game.sound.play(Paths.sound("game/countdown/base/intro1"));
-                case 3: Game.sound.play(Paths.sound("game/countdown/base/introGo"));
-            }
-        }, 4);
-    }
+		call("onCreatePost");
+	}
 
-    override function update(elapsed:Float) {
-        super.update(elapsed);
+	public function startCountdown() {
+		var swagCounter:Int = 0;
+		startTimer = new Timer().start(Conductor.crochet / 1000, (timer:Timer) -> {
+			switch (swagCounter++) {
+				case 0: Game.sound.play(Paths.sound("game/countdown/base/intro3"));
+				case 1: Game.sound.play(Paths.sound("game/countdown/base/intro2"));
+				case 2: Game.sound.play(Paths.sound("game/countdown/base/intro1"));
+				case 3: Game.sound.play(Paths.sound("game/countdown/base/introGo"));
+			}
+		}, 4);
+	}
 
-        if(!endingSong) {
-            Conductor.position += elapsed * 1000;
-            call("onUpdate", [elapsed]);
-        }
+	override function update(elapsed:Float) {
+		super.update(elapsed);
 
-        if(startingSong) {
-            if(Conductor.position >= 0)
-                startSong();
-        }
+		if (!endingSong) {
+			Conductor.position += elapsed * 1000;
+			call("onUpdate", [elapsed]);
+		}
 
-        Game.camera.zoom = MathUtil.lerp(Game.camera.zoom, defaultCamZoom, 0.05, true);
-        camHUD.zoom = MathUtil.lerp(camHUD.zoom, 1, 0.05, true);
+		if (startingSong) {
+			if (Conductor.position >= 0)
+				startSong();
+		}
 
-        if(unspawnNotes[0] != null && !endingSong) {
-            while(unspawnNotes[0] != null && unspawnNotes[0].strumTime <= Conductor.position + (2500 / (scrollSpeed / Game.timeScale)))
-                notes.add(unspawnNotes.shift());
-        }
+		Game.camera.zoom = MathUtil.lerp(Game.camera.zoom, defaultCamZoom, 0.05, true);
+		camHUD.zoom = MathUtil.lerp(camHUD.zoom, 1, 0.05, true);
 
-        keyShit();
-    }
+		if (unspawnNotes[0] != null && !endingSong) {
+			while (unspawnNotes[0] != null && unspawnNotes[0].strumTime <= Conductor.position + (2500 / (scrollSpeed / Game.timeScale)))
+				notes.add(unspawnNotes.shift());
+		}
 
-    public var keyBinds:Array<Int> = [
-        Keys.S,
-        Keys.D,
-        Keys.K,
-        Keys.L
-    ];
+		keyShit();
+	}
 
-    public function goodNoteHit(note:Note) {
-        if(note.mustPress) {
-            if(!note.isSustainNote) {
-                var ratingSpr:Sprite = new Sprite((Game.width * 0.55) - 40, (Game.height * 0.5) - 60).loadGraphic(Paths.image('gameplay/ratings/sick'));
-                ratingSpr.scale.set(0.7, 0.7);
-                ratingSpr.updateHitbox();
-                ratingSpr.alpha = 1;
-                ratingSpr.acceleration.y = 550;
-                ratingSpr.velocity.y = -Game.random.int(140, 175);
-                ratingSpr.velocity.x = -Game.random.int(0, 10);
-                add(ratingSpr);
+	public var keyBinds:Array<Int> = [Keys.S, Keys.D, Keys.K, Keys.L];
 
-                Tween.tween(ratingSpr, {alpha: 0}, 0.2, {
-                    onComplete: (tween:Tween) -> {
-                        ratingSpr.kill();
-                    },
-                    startDelay: Conductor.crochet * 0.001
-                });
-            }
-        }
-        note.strumLine.members[note.noteData].playAnim("confirm", true);
-        notes.deleteNote(note);
-    }
+	public function goodNoteHit(note:Note) {
+		if (note.mustPress) {
+			if (!note.isSustainNote) {
+				var ratingSpr:Sprite = new Sprite((Game.width * 0.55) - 40, (Game.height * 0.5) - 60).loadGraphic(Paths.image('gameplay/ratings/sick'));
+				ratingSpr.scale.set(0.7, 0.7);
+				ratingSpr.updateHitbox();
+				ratingSpr.alpha = 1;
+				ratingSpr.acceleration.y = 550;
+				ratingSpr.velocity.y = -Game.random.int(140, 175);
+				ratingSpr.velocity.x = -Game.random.int(0, 10);
+				add(ratingSpr);
 
-    public function keyShit() {
-        if(SettingsAPI.botplay) return;
+				Tween.tween(ratingSpr, {alpha: 0}, 0.2, {
+					onComplete: (tween:Tween) -> {
+						ratingSpr.kill();
+					},
+					startDelay: Conductor.crochet * 0.001
+				});
+			}
+		}
+		note.strumLine.members[note.noteData].playAnim("confirm", true);
+		notes.deleteNote(note);
+	}
 
-        for(i => bind in keyBinds) {
-            if(Game.keys.justPressed(bind))
-                playerStrums.members[i].playAnim("pressed", true);
-        }
+	public function keyShit() {
+		if (SettingsAPI.botplay)
+			return;
 
-        var possibleNotes:Array<Note> = [];
-        notes.forEachAlive((note:Note) -> {
-            if(!note.mustPress || !note.canBeHit || note.isSustainNote) return;
-            possibleNotes.push(note);
-        });
-        possibleNotes.sort((a, b) -> Std.int(a.strumTime - b.strumTime));
+		for (i => bind in keyBinds) {
+			if (Game.keys.justPressed(bind))
+				playerStrums.members[i].playAnim("pressed", true);
+		}
 
-        var hitBlocked:Array<Bool> = [for(_ in 0...SONG.keyCount) false];
-        for(note in possibleNotes) {
-            if(!Game.keys.justPressed(keyBinds[note.noteData]) || hitBlocked[note.noteData]) continue;
-            hitBlocked[note.noteData] = true;
-            goodNoteHit(note);
-        }
+		var possibleNotes:Array<Note> = [];
+		notes.forEachAlive((note:Note) -> {
+			if (!note.mustPress || !note.canBeHit || note.isSustainNote)
+				return;
+			possibleNotes.push(note);
+		});
+		possibleNotes.sort((a, b) -> Std.int(a.strumTime - b.strumTime));
 
-        for(i => bind in keyBinds) {
-            if(Game.keys.justReleased(bind))
-                playerStrums.members[i].playAnim("static", true);
-        }
-    }
+		var hitBlocked:Array<Bool> = [for (_ in 0...SONG.keyCount) false];
+		for (note in possibleNotes) {
+			if (!Game.keys.justPressed(keyBinds[note.noteData]) || hitBlocked[note.noteData])
+				continue;
+			hitBlocked[note.noteData] = true;
+			goodNoteHit(note);
+		}
 
-    override function beatHit(curBeat:Int) {
-        super.beatHit(curBeat);
+		for (i => bind in keyBinds) {
+			if (Game.keys.justReleased(bind))
+				playerStrums.members[i].playAnim("static", true);
+		}
+	}
 
-        if(startingSong || endingSong) return;
+	override function beatHit(curBeat:Int) {
+		super.beatHit(curBeat);
 
-        call("onBeatHit", [curBeat]);
+		if (startingSong || endingSong)
+			return;
 
-        var section = SONG.notes[Math.floor(curBeat / 4)];
-        if (section != null && section.mustHitSection)
-            camFollow.setPosition(770 + 411 / 2 - 100, 450 + 412 / 2 - 100); // 875.5, 556
-        else
-            camFollow.setPosition(100 + 429 / 2 + 150, 100 + 767 / 2 - 100); // 464.5, 385.5
+		call("onBeatHit", [curBeat]);
 
-        if(curBeat % 4 == 0) {
-            Game.camera.zoom += 0.015;
-            camHUD.zoom += 0.03;
-        }
+		var section = SONG.notes[Math.floor(curBeat / 4)];
+		if (section != null && section.mustHitSection)
+			camFollow.setPosition(770 + 411 / 2 - 100, 450 + 412 / 2 - 100); // 875.5, 556
+		else
+			camFollow.setPosition(100 + 429 / 2 + 150, 100 + 767 / 2 - 100); // 464.5, 385.5
 
-        var resyncTime:Float = 20 * Game.timeScale;
-        if(Math.abs(Conductor.position - inst.time) > resyncTime || Math.abs(Conductor.position - vocals.time) > resyncTime)
-            resyncVocals();
-    }
+		if (curBeat % 4 == 0) {
+			Game.camera.zoom += 0.015;
+			camHUD.zoom += 0.03;
+		}
 
-    public function resyncVocals() {
-        inst.pause();
-        vocals.pause();
+		#if linux
+		var resyncTime:Float = 30 * Game.timeScale;
+		#else
+		var resyncTime:Float = 20 * Game.timeScale;
+		#end
+		if (Math.abs(Conductor.position - inst.time) > resyncTime || Math.abs(Conductor.position - vocals.time) > resyncTime)
+			resyncVocals();
+	}
 
-        inst.time = vocals.time = Conductor.position;
+	public function resyncVocals() {
+		Logs.trace('Resynced vocals at song position: ${Conductor.position} with an inst and vocals times of: ${inst.time}, ${vocals.time}', INFO);
 
-        inst.play();
-        vocals.play();
-    }
+		inst.pause();
+		vocals.pause();
 
-    public function startSong() {
-        startingSong = false;
-        inst.time = vocals.time = Conductor.position = 0;
-        inst.play();
-        vocals.play();
-    }
+		inst.time = vocals.time = Conductor.position;
 
-    public function endSong() {
-        Game.timeScale = 1;
-        endingSong = true;
-        Game.switchScene(new MainMenu());
-    }
+		inst.play();
+		vocals.play();
+	}
 
-    override function destroy() {
-        for (script in scripts)
-            script.destroy();
-        super.destroy();
-    }
+	public function startSong() {
+		startingSong = false;
+		inst.time = vocals.time = Conductor.position = 0;
+		inst.play();
+		vocals.play();
+	}
 
-    function call(method:String, ?parameters:Array<Dynamic>) {
-        for (script in scripts)
-            script.call(method, parameters);
-    }
+	public function endSong() {
+		Game.timeScale = 1;
+		endingSong = true;
+		Game.switchScene(new MainMenu());
+	}
+
+	override function destroy() {
+		for (script in scripts)
+			script.destroy();
+		super.destroy();
+	}
+
+	function call(method:String, ?parameters:Array<Dynamic>) {
+		for (script in scripts)
+			script.call(method, parameters);
+	}
 }
